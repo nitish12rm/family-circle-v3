88d25773ed6960bcad8d2be187a1a88652b91058
nitish <nitish12rm@gmail.com>
2026-03-27 02:34:24 +0530

    "the how do i know" doesnt go away after confirming fix

--- src/components/tree/TreeView.tsx
+++ src/components/tree/TreeView.tsx
@@ -905,7 +905,6 @@ export default function TreeView() {
           }}
           onRelAdded={() => {
             loadTree();
-            setSelectedMember(null);
           }}
         />
       )}
